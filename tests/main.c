140f51b4e7e2bf84c4b0b1111f75352f144d5736
Omer Akram <omer@xconn.io>
2025-08-30 23:57:34 +0500

    add support for json serializer

--- tests/main.c
+++ tests/main.c
@@ -11,6 +11,7 @@ void test_json(void);
 int main(void) {
     test_cbor();
     test_msgpack();
+    test_json();
     return 0;
 }
 
@@ -30,6 +31,21 @@ void test_cbor(void) {
 }
 
 void test_msgpack(void) {
+    const Serializer *serializer = cbor_serializer_new();
+    const Registered *registered = registered_new(1, 2);
+    const Message *msg = (Message *) registered;
+
+    const ByteArray arr = serializer->serialize(serializer, msg);
+
+    const Message *deserialized = serializer->deserialize(serializer, arr.data, arr.len);
+    assert(deserialized != NULL);
+
+    const Registered *r = (Registered *) deserialized;
+    assert(r != NULL);
+    assert(r->base.message_type(deserialized) == 65);
+}
+
+void test_json(void) {
     const Serializer *serializer = cbor_serializer_new();
     const Registered *registered = registered_new(1, 2);
     const Message *msg = (Message *) registered;
