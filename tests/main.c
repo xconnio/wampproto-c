2ff31fd59bdacd014f25c0644591da8485d683f0
Omer Akram <omer@xconn.io>
2025-08-31 18:25:41 +0500

    DRY test code

--- tests/main.c
+++ tests/main.c
@@ -16,50 +16,43 @@ int main(void)
     return 0;
 }
 
-void test_cbor(void)
+const Message *create_registered_message(void)
 {
-    const Serializer *serializer = cbor_serializer_new();
     const Registered *registered = registered_new(1, 2);
     const Message *msg = (Message *)registered;
+    return msg;
+}
 
+const void *check_serializer(const Serializer *serializer, const Message *msg)
+{
     const Bytes arr = serializer->serialize(serializer, msg);
-
     const Message *deserialized = serializer->deserialize(serializer, arr);
     assert(deserialized != NULL);
 
     const Registered *r = (Registered *)deserialized;
     assert(r != NULL);
     assert(r->base.message_type(deserialized) == 65);
+
+    return NULL;
+}
+
+void test_cbor(void)
+{
+    const Serializer *serializer = cbor_serializer_new();
+    const Message *msg = create_registered_message();
+    check_serializer(serializer, msg);
 }
 
 void test_msgpack(void)
 {
     const Serializer *serializer = msgpack_serializer_new();
-    const Registered *registered = registered_new(1, 2);
-    const Message *msg = (Message *)registered;
-
-    const Bytes arr = serializer->serialize(serializer, msg);
-
-    const Message *deserialized = serializer->deserialize(serializer, arr);
-    assert(deserialized != NULL);
-
-    const Registered *r = (Registered *)deserialized;
-    assert(r != NULL);
-    assert(r->base.message_type(deserialized) == 65);
+    const Message *msg = create_registered_message();
+    check_serializer(serializer, msg);
 }
 
 void test_json(void)
 {
     const Serializer *serializer = json_serializer_new();
-    const Registered *registered = registered_new(1, 2);
-    const Message *msg = (Message *)registered;
-
-    const Bytes arr = serializer->serialize(serializer, msg);
-
-    const Message *deserialized = serializer->deserialize(serializer, arr);
-    assert(deserialized != NULL);
-
-    const Registered *r = (Registered *)deserialized;
-    assert(r != NULL);
-    assert(r->base.message_type(deserialized) == 65);
+    const Message *msg = create_registered_message();
+    check_serializer(serializer, msg);
 }
