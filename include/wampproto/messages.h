e7caff6a6584947917241c8ffb70cd45196e8052
Omer Akram <omer@xconn.io>
2025-09-16 17:03:25 +0500

    make sure to export all message types

--- include/wampproto/messages.h
+++ include/wampproto/messages.h
@@ -1,6 +1,9 @@
 #ifndef WAMPPROTO_MESSAGES_H
 #define WAMPPROTO_MESSAGES_H
 
+#include "wampproto/messages/authenticate.h"
+#include "wampproto/messages/call.h"
+#include "wampproto/messages/challenge.h"
 #include "wampproto/messages/message.h"
 #include "wampproto/messages/registered.h"
 
