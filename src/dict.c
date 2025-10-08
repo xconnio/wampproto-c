4c5705c9cf26d2224ffdf0631a96e3d41ae8c878
Omer Akram <omer@xconn.io>
2025-10-08 20:03:03 +0500

    replace adhoc dict with uthash impl (#22)

--- src/dict.c
+++ src/dict.c
@@ -2,120 +2,90 @@
 #include "wampproto/value.h"
 
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define TABLE_SIZE 1024
+#include "uthash.h"
 
-uint64_t fnv1a(const char *key)
+Dict *create_dict()
 {
-    uint64_t hash = 1469598103934665603ULL;
-    while (*key)
-    {
-        hash ^= (unsigned char)(*key++);
-        hash *= 1099511628211ULL;
-    }
-    return hash;
-}
 
-Dict *create_dict(size_t size)
-{
     Dict *dict = malloc(sizeof(Dict));
-    dict->size = size;
+    if (!dict)
+        return NULL;
+
+    dict->table = NULL;
     dict->count = 0;
-    dict->buckets = calloc(dict->size, sizeof(Entry *));
     return dict;
-}
-
-void resize_dict(Dict *dict, size_t new_size)
-{
-    Entry **new_buckets = calloc(new_size, sizeof(Entry *));
-
-    for (size_t i = 0; i < dict->size; i++)
-    {
-        Entry *curr = dict->buckets[i];
-        while (curr)
-        {
-            Entry *next = curr->next;
-
-            // Recompute index in new table
-            uint64_t hash = fnv1a(curr->key);
-            size_t new_index = hash % new_size;
-
-            // Insert at head of new bucket
-            curr->next = new_buckets[new_index];
-            new_buckets[new_index] = curr;
-
-            curr = next;
-        }
-    }
-    free(dict->buckets);
-    dict->buckets = new_buckets;
-    dict->size = new_size;
 }
 
 void dict_insert(Dict *dict, const char *key, Value *value)
 {
-    uint64_t hash = fnv1a(key);
-    size_t index = hash % dict->size;
+    if (!dict || !key)
+        return;
 
-    Entry *curr = dict->buckets[index];
-    while (curr)
+    Entry *entry = NULL;
+    HASH_FIND_STR(dict->table, key, entry);
+
+    if (entry)
     {
-        if (strcmp(curr->key, key) == 0)
-        {
-            value_free(curr->value);
-            curr->value = value;
-            return;
-        }
-        curr = curr->next;
+        // Replace existing value
+        value_free(entry->value);
+        entry->value = value;
+        return;
     }
 
-    Entry *new_entry = malloc(sizeof(Entry));
-    new_entry->key = strdup(key);
-    new_entry->value = value;
-    new_entry->next = dict->buckets[index];
-    dict->buckets[index] = new_entry;
+    // Create new entry
+    entry = malloc(sizeof(*entry));
+    if (!entry)
+        return;
 
+    entry->key = strdup(key);
+    entry->value = value;
+
+    HASH_ADD_KEYPTR(hh, dict->table, entry->key, strlen(entry->key), entry);
     dict->count++;
-
-    if ((double)dict->count / dict->size > 0.7)
-    {
-        resize_dict(dict, dict->size * 2);
-    }
 }
 
 Value *dict_get(Dict *dict, const char *key)
 {
-    uint64_t hash = fnv1a(key);
-    size_t index = hash % dict->size;
+    if (!dict || !key)
+        return NULL;
 
-    Entry *curr = dict->buckets[index];
-    while (curr)
+    Entry *entry = NULL;
+    HASH_FIND_STR(dict->table, key, entry);
+    return entry ? entry->value : NULL;
+}
+
+void dict_remove(Dict *dict, const char *key)
+{
+    if (!dict || !key)
+        return;
+
+    Entry *entry = NULL;
+    HASH_FIND_STR(dict->table, key, entry);
+    if (entry)
     {
-        if (strcmp(curr->key, key) == 0)
-        {
-            return curr->value;
-        }
-        curr = curr->next;
+        HASH_DEL(dict->table, entry);
+        free(entry->key);
+        value_free(entry->value);
+        free(entry);
+        dict->count--;
     }
-    return NULL; // not found
 }
 
 void dict_free(Dict *dict)
 {
-    for (size_t i = 0; i < dict->size; i++)
+    if (!dict)
+        return;
+
+    Entry *curr, *tmp;
+    HASH_ITER(hh, dict->table, curr, tmp)
     {
-        Entry *curr = dict->buckets[i];
-        while (curr)
-        {
-            Entry *tmp = curr;
-            curr = curr->next;
-            free(tmp->key);
-            free(tmp);
-        }
+        HASH_DEL(dict->table, curr);
+        free(curr->key);
+        value_free(curr->value);
+        free(curr);
     }
-    free(dict->buckets);
     free(dict);
 }
